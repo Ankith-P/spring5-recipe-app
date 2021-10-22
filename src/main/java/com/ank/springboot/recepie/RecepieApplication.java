3a5054d486ac241a7b5cfe60cc9d00c479db24de
ankith.prakash <ankith.prakash@metricstream.com>
2021-10-22 23:13:30 +0530

    Completing the assignment
    Signed-off-by: ankith.prakash <ankith.prakash@metricstream.com>

--- src/main/java/com/ank/springboot/recepie/RecepieApplication.java
+++ src/main/java/com/ank/springboot/recepie/RecepieApplication.java
@@ -3,10 +3,8 @@ package com.ank.springboot.recepie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ank.springboot.domain"})
 @EntityScan(basePackages = {"com.ank.springboot"})
 public class RecepieApplication {
 
